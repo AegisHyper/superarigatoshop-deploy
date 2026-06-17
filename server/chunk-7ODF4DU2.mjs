c082b31882e436c2c6e07822540d7c7604dfed97
Luis Núñez <panicjoker03@outlook.com>
2026-06-17 11:33:22 -0600

    Auto-generated commit

--- server/chunk-7ODF4DU2.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Ob as a,Pb as s,kc as i,pb as r}from"./chunk-O7Z3MC4V.mjs";import"./chunk-5R5OPJSI.mjs";var n=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=r({type:t,selectors:[["app-order-details"]],decls:2,vars:0,template:function(e,d){e&1&&(a(0,"p"),i(1,"order-details works!"),s())},encapsulation:2})};export{n as OrderDetails};
